5af8f33bdabae8bf0cfe20e4fa329fa1c6cf9587
arch-DJ <divyanshjamuaar@gmail.com>
2018-03-29 12:09:17 +0530

    Add populate to teacher login

--- api/routes/teacherlogin.js
+++ api/routes/teacherlogin.js
@@ -5,12 +5,18 @@ const mongoose = require("mongoose");
 const Teacher = require("../models/teacher");
 
 router.post("/", (req, res, next) => {
-  Teacher.findOne({"userid":req.body.userid}, (error, data1)=>{
+  Teacher.findOne({"userid":req.body.userid}).populate({
+   path: 'subjects',
+   populate: {
+     path: 'room',
+     model: 'Classroom'
+   }}).exec(function (error, data1) {
         if(data1 != null) {
             var ans = data1.password;
 
             if (ans == req.body.password)  {
               res.status(200);
+
               res.json(data1);
             }
 
@@ -22,7 +28,6 @@ router.post("/", (req, res, next) => {
         }
         else{
             res.status(404);
-            //res.json({'msg' : 'Error', 'id' : new mongoose.Types.ObjectId()});
             res.json({'msg' : 'Error'});
         }
     });
